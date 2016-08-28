5f4ef0b2a60d982a38fed14877df467d9dfba102
jiasm <jiashunming@outlook.com>
2016-08-28 13:25:17 +0800

    update

---
+++ deep-copy.js
@@ -0,0 +1,35 @@
+'use strict'
+
+function copy (orig) {
+  
+  // copy has same prototype as orig
+  let copy = Object.create(Object.getPrototypeOf(orig))
+
+  // copy has all of orig's properties
+  copyOwnPropertiesFrom(copy, orig)
+
+  return copy
+}
+
+function copyOwnPropertiesFrom (target, source) {
+  Object.getOwnPropertyNames(source)
+  .forEach(propKey => {
+    let desc = Object.getOwnPropertyDescriptor(source, propKey)
+	Object.defineProperty(target, propKey, desc)
+  })
+
+  return target
+}
+
+let obj = {
+  name: 'niko',
+  friends: [
+    'roman'
+  ]
+}
+
+let newObj = copy(obj)
+
+newObj.friends.push('bellic')
+
+console.log(obj.friends.join('|'))
