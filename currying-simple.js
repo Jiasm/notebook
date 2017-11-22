8867ea41f8f9554ce886070173400cd43e2b0933
贾顺名(shunming jia) <jiashunming@outlook.com>
2017-11-22 15:57:48 +0800

    Update currying-simple.js

--- currying-simple.js
+++ currying-simple.js
@@ -2,8 +2,8 @@
  * 柯里化的一种实现方式
  */
 
-function currying (val, last = 0) {
-  val = +val + +last
+function currying (...arg) {
+  val = arg.reduce((cur, old) => cur + old, 0)
   return Object.assign(currying.bind(this, val), {
     valueOf () {
       return val
@@ -19,4 +19,4 @@ function currying (val, last = 0) {
 var a = currying(1)(2)
 
 // 因为console.log实现的问题。。所以必须在前边添加一个+来确保能够得到正确的输出
-console.log(+a, +a(4), +a(5))
+console.log(+a, +a(4), +a(5, 6))
