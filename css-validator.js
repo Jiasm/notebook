a4b5f5963ce092ac8669d7cb2bf98f3dfbd45145
jiashunming <jiashunming@outlook.com>
2018-03-06 14:49:17 +0800

    add css validator

---
+++ css-validator.js
@@ -0,0 +1,29 @@
+function supports (key, value) {
+  let originKey = key
+  key = key.replace(/-(\w)/g, (_, $1) => $1.toUpperCase()) // 转成驼峰
+  if (!(key in document.documentElement.style)) return new Error('unsupported rule')
+
+  // 如果存在value，则去检查是否支持对应的值
+  if (value) {
+    let _tmp = document.createElement('div')
+    _tmp.style[key] = value
+
+    // 创建一个DOM元素，这样getComputedStyle才会生效
+    document.body.appendChild(_tmp)
+
+    let result = getComputedStyle(_tmp)[key] !== value
+
+    // 检查完毕后移除该元素
+    document.body.removeChild(_tmp)
+
+    if (result) return new Error(`unsupported value in: ${originKey}`)
+  }
+
+  return true
+}
+
+// test code
+console.log(supports('border-image'))
+console.log(supports('border-image1'))
+console.log(supports('background-size', 'cover'))
+console.log(supports('background-size', 'cover1'))
