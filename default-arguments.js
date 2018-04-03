be2c0cf70b932a081ba032941b8f9ddeca6e3f96
jiashunming <jiashunming@outlook.com>
2018-04-03 10:04:16 +0800

    update

---
+++ default-arguments.js
@@ -0,0 +1,27 @@
+function func_es5 (callback) {
+  callback = callback || defaultFunction
+
+  return callback
+}
+
+function func_es6 (callback = defaultFunction) {
+  return callback
+}
+
+function defaultFunction () {}
+
+// test es5
+
+console.log('func_es5()      ', typeof func_es5())
+console.log('func_es5(0)     ', typeof func_es5(0))
+console.log('func_es5(false) ', typeof func_es5(false))
+console.log('func_es5(\'\')    ', typeof func_es5(''))
+console.log('func_es5(null)  ', typeof func_es5(null))
+
+// test es6
+
+console.log('func_es6()      ', typeof func_es6())
+console.log('func_es6(0)     ', typeof func_es6(0))
+console.log('func_es6(false) ', typeof func_es6(false))
+console.log('func_es6(\'\')    ', typeof func_es6(''))
+console.log('func_es6(null)  ', typeof func_es6(null))
