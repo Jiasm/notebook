e5f0f475f4211730c92e985017fb8ef4d805030d
jiashunming <jiashunming@outlook.com>
2018-04-16 21:32:50 +0800

    update

--- non-intersection.js
+++ non-intersection.js
@@ -16,11 +16,20 @@
   }, {
     id: 222,
     name: 'bbb'
+  }, {
+    id: 444,
+    name: 'ddd'
   }]
 
   function filter (arr1, arr2, rules) {
     return arr1.filter(item1 => !arr2.some(item2 => rules.every(key => item1[key] === item2[key])))
   }
 
-  console.log(filter(arr1, arr2, ['id', 'name']))
+  function build (arr1, arr2, rules) {
+    return [].concat(filter(arr1, arr2, rules), filter(arr2, arr1, rules))
+  }
+
+  let result = build(arr1, arr2, ['id', 'name'])
+
+  console.log(result)
 })()
