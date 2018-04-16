b473d59da2641aff807179c3d47475865864c453
jiashunming <jiashunming@outlook.com>
2018-04-16 20:51:35 +0800

    添加非交集的获取

---
+++ non-intersection.js
@@ -0,0 +1,26 @@
+(() => {
+  var arr1 = [{
+    id: 111,
+    name: 'aaa'
+  }, {
+    id: 222,
+    name: 'bbb'
+  }, {
+    id: 333,
+    name: 'ccc'
+  }]
+
+  var arr2 = [{
+    id: 111,
+    name: 'aaa'
+  }, {
+    id: 222,
+    name: 'bbb'
+  }]
+
+  function filter (arr1, arr2, rules) {
+    return arr1.filter(item1 => !arr2.some(item2 => rules.every(key => item1[key] === item2[key])))
+  }
+
+  console.log(filter(arr1, arr2, ['id', 'name']))
+})()
