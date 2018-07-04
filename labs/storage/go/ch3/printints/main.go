59529dbba83389ca64ed51c8a452932544e1f2c8
jiashunming <jiashunming@outlook.com>
2018-07-04 09:48:24 +0800

    update

---
+++ labs/storage/go/ch3/printints/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"bytes"
+)
+
+func main() {
+	fmt.Println(intsToString([]int{1, 2, 3}))
+}
+
+func intsToString(values []int) string {
+	var buf bytes.Buffer
+	buf.WriteByte('[')
+
+	for i, v := range values {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+
+		fmt.Fprintf(&buf, "%d", v)
+	}
+
+	buf.WriteByte(']')
+
+	return buf.String()
+}
