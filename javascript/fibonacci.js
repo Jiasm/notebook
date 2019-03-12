38174e674f5681c01169b3e02273409d692c0a95
jiasm <jiashunming@outlook.com>
2019-03-12 10:19:23 +0800

    doc: change code

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,4 +1,9 @@
-function fibonacci (len, res = [0, 1, 1], cursor = res.length) {
+function fibonacci (len, res = [0, 1], cursor = res.length) {
   if (len <= cursor) return res.slice(0, len)
   else return c(len, [...res, res[cursor - 1] + res[cursor - 2]])
 }
+
+console.log(fibonacci(-2))
+console.log(fibonacci(1))
+console.log(fibonacci(2))
+console.log(fibonacci(10))
