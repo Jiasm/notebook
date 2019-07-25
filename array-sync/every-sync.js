63c439e9f421982c2bf4f0a36a657591531c88f9
jiasm <jiashunming@outlook.com>
2019-07-25 14:23:46 +0800

    feat: remove unuse arguments

--- array-sync/every-sync.js
+++ array-sync/every-sync.js
@@ -1,4 +1,4 @@
-Array.prototype.everySync = async function (callback, thisArg) {
+Array.prototype.everySync = async function (callback) {
   for (let [index, item] of Object.entries(this)) {
     if (!await callback(item, index, this)) return false
   }
