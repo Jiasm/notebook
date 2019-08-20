317e832a04d0441a22b42576e56d6e8b3c88e1f2
jiasm <jiashunming@outlook.com>
2019-08-20 16:33:01 +0800

    style: beauty file

--- javascript/translate-tree-data.js
+++ javascript/translate-tree-data.js
@@ -6,18 +6,18 @@
  *  @author jiasm
  */
 
-var jsonData = [
-  {"provname":"广东","cityname":"深圳","localname":"罗田","deptname":"保险1","struid":"C0092","manager":"小飞","telephone":"020-110"},
-  {"provname":"广东","cityname":"深圳","localname":"福田","deptname":"保险2","struid":"C0092","manager":"小张","telephone":"020-110"},
-  {"provname":"广东","cityname":"深圳","localname":"福田","deptname":"保险3","struid":"C0092","manager":"小名","telephone":"020-110"},
-  {"provname":"广东","cityname":"东莞","localname":"厚街","deptname":"保险4","struid":"C0092","manager":"小飞","telephone":"020-110"},
-  {"provname":"广东","cityname":"广州","localname":"白云","deptname":"保险5","struid":"C0092","manager":"小张","telephone":"020-110"},
-  {"provname":"广东","cityname":"广州","localname":"荔湾","deptname":"保险6","struid":"C0092","manager":"小张","telephone":"020-110"},
-  {"provname":"广东","cityname":"东莞","localname":"厚街","deptname":"保险7","struid":"C0092","manager":"小名","telephone":"020-110"},
-  {"provname":"广东","cityname":"惠州","localname":"厚街","deptname":"保险7","struid":"C0092","manager":"小名","telephone":"020-110"},
-  {"provname":"广东","cityname":"东莞","localname":"厚街","deptname":"保险7","struid":"C0092","manager":"小名","telephone":"020-110"},
-  {"provname":"广东","cityname":"惠州","localname":"厚街","deptname":"保险7","struid":"C0092","manager":"小名","telephone":"020-110"},
-  {"provname":"广东","cityname":"惠州","localname":"厚街","deptname":"保险7","struid":"C0092","manager":"小名","telephone":"020-110"}
+const jsonData = [
+  { provname: '广东', cityname: '深圳', localname: '罗田', deptname: '保险1', struid: 'C0092', manager: '小飞', telephone: '020-110' },
+  { provname: '广东', cityname: '深圳', localname: '福田', deptname: '保险2', struid: 'C0092', manager: '小张', telephone: '020-110' },
+  { provname: '广东', cityname: '深圳', localname: '福田', deptname: '保险3', struid: 'C0092', manager: '小名', telephone: '020-110' },
+  { provname: '广东', cityname: '东莞', localname: '厚街', deptname: '保险4', struid: 'C0092', manager: '小飞', telephone: '020-110' },
+  { provname: '广东', cityname: '广州', localname: '白云', deptname: '保险5', struid: 'C0092', manager: '小张', telephone: '020-110' },
+  { provname: '广东', cityname: '广州', localname: '荔湾', deptname: '保险6', struid: 'C0092', manager: '小张', telephone: '020-110' },
+  { provname: '广东', cityname: '东莞', localname: '厚街', deptname: '保险7', struid: 'C0092', manager: '小名', telephone: '020-110' },
+  { provname: '广东', cityname: '惠州', localname: '厚街', deptname: '保险7', struid: 'C0092', manager: '小名', telephone: '020-110' },
+  { provname: '广东', cityname: '东莞', localname: '厚街', deptname: '保险7', struid: 'C0092', manager: '小名', telephone: '020-110' },
+  { provname: '广东', cityname: '惠州', localname: '厚街', deptname: '保险7', struid: 'C0092', manager: '小名', telephone: '020-110' },
+  { provname: '广东', cityname: '惠州', localname: '厚街', deptname: '保险7', struid: 'C0092', manager: '小名', telephone: '020-110' },
 ]
 
 /**
